ffa4efb7837ad3e3ce7e9079e5b368fe0e9d0570
René Moll <github@r-moll.nl>
2024-02-07 16:41:29 +0100

    fix broken test-case

--- tests/ref/bootstrap.cmake
+++ tests/ref/bootstrap.cmake
@@ -1,4 +1,3 @@
-
 include(FetchContent)
 
 FetchContent_Declare(
@@ -6,6 +5,18 @@ FetchContent_Declare(
   GIT_REPOSITORY https://github.com/renemoll/bob-cmake.git
   GIT_TAG        origin/main
   GIT_SHALLOW    true
+  SOURCE_DIR     ${CMAKE_BINARY_DIR}/bob-cmake
 )
 
 FetchContent_MakeAvailable(bob-cmake)
+
+list(PREPEND CMAKE_MODULE_PATH "${CMAKE_BINARY_DIR}/bob-cmake")
+
+#
+# Todo: fetch a specific version
+#
+# FetchContent_Declare(
+#   bob-cmake
+#   URL https://github.com/renemoll/bob-cmake/archive/refs/tags/0.0.1.tar.gz
+# )
+# 
