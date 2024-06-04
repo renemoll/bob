9e2b171dc700fddac515119c8dcd76e3008354c1
René Moll <github@r-moll.nl>
2024-06-04 22:51:21 +0200

    bob: add __main__

--- bob/__init__.py
+++ bob/__init__.py
@@ -1,6 +1,6 @@
 """API for Bob."""
 
+__version__ = "0.2.5"
+
 from bob.api import BuildConfig, Command
 from bob.bob import bob
-
-__version__ = "0.2.5"
---
+++ bob/__main__.py
@@ -0,0 +1,5 @@
+"""Module entry-point for Bob."""
+from bob.cli import main
+
+if __name__ == "__main__":
+    raise SystemExit(main())
